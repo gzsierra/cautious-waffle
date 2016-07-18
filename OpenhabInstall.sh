612e985e8c42c0de33127621a00133944eb884fb
Nicolas Nadeau <niconadeau0@gmail.com>
2016-07-18 14:50:57 -0400

    Fix naming file

--- OpenhabInstall.sh
+++ OpenhabInstall.sh
@@ -8,11 +8,13 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+echo "\n######################\n## Java Setup \n######################\n"
 # JAVA setup
 apt-add-repository ppa:webupd8team/java
 apt update
-apt install oracle-java8-installer
+apt install -y oracle-java8-installer
 
+echo "\n######################\n## MySQL Setup \n######################\n"
 # MySQL setup
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password openhab'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password openhab'
@@ -22,39 +24,44 @@ mysql -u root --password='openhab' -e "CREATE DATABASE OpenHAB"
 mysql -u root --password='openhab' -e "CREATE USER 'openhab'@'localhost' IDENTIFIED BY 'yourpassword'"
 mysql -u root --password='openhab' -e "GRANT ALL PRIVILEGES ON OpenHAB.* TO 'openhab'@'localhost'"
 
+echo "\n######################\n## Mosquitto Setup \n######################\n"
 # Mosquitto setup
 apt-add-repository ppa:mosquitto-dev/mosquitto-ppa
 apt update
-apt install mosquitto
+apt install -y mosquitto
 
 echo 'allow_anonymous = true' >> /etc/mosquitto/mosquitto.conf
 /etc/init.d/mosquitto restart
 
+echo "\n######################\n## CoAP Setup \n######################\n"
 # CoAP setup
-# apt install git
+apt install python3-pip
+pip3 install aiocoap
+
 git clone https://github.com/gzsierra/pycoap/ /opt/pycoap
 ln -s /opt/pycoap/pyServer.py /usr/bin/coapServer
 ln -s /opt/pycoap/pyGet.py /usr/bin/pyGet
 
 ## CoAP Server Setup as a Service
-update-rc.d coapServer defaults
 systemctl enable coapServer
+service coapServer start
 
+echo "\n######################\n## OpenHAB Setup \n######################\n"
 # OpenHAB setup
 wget -qO - 'https://bintray.com/user/downloadSubjectPublicKey?username=openhab' |  apt-key add -
 echo "deb http://dl.bintray.com/openhab/apt-repo stable main" |  tee /etc/apt/sources.list.d/openhab.list
 apt update
-apt install openhab-runtime
+apt install -y openhab-runtime
 
-apt install openhab-addon-binding-astro \
-            openhab-addon-binding-exec \
-            openhab-addon-binding-http \
-            openhab-addon-binding-mqtt \
-            openhab-addon-binding-mqttitude \
-            openhab-addon-binding-networkhealth \
-            openhab-addon-binding-wol \
-            openhab-addon-binding-zwave \
-            openhab-addon-persistence-mysql
+apt install -y openhab-addon-binding-astro \
+               openhab-addon-binding-exec \
+               openhab-addon-binding-http \
+               openhab-addon-binding-mqtt \
+               openhab-addon-binding-mqttitude \
+               openhab-addon-binding-networkhealth \
+               openhab-addon-binding-wol \
+               openhab-addon-binding-zwave \
+               openhab-addon-persistence-mysql
 
 # Custom config file
 cp file/coapServer /etc/init.d/coapServer
